5de4c9d90d32b8c6c90da673e1cce6c339c65f0f
KingBoyAndGirl <1782635977@qq.com>
2023-10-16 15:09:48 +0800

    声明

--- src/components/reception/footer.jsx
+++ src/components/reception/footer.jsx
@@ -7,15 +7,12 @@ const Footer = () => {
     <footer className='footer'>
       <div>
         <div>
-        部分资源来自网络，如有侵权请联系
+          所有作品均由小王摄影拍摄（抖音：XWSY688   小红书：小王摄影），转载必须征得本人同意后，方可转载！！！
         <code>
         <a href="mailto:mokingboygirl@gmail.com"  rel="noopener noreferrer">
           mokingboygirl@gmail.com</a>
         </code>
-
-        处理
         </div>
-
         <div>&copy; 小王摄影 2022-2023</div>
       </div>
 
