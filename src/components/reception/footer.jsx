b772a484c5fe6f712f251fe2f276cf55aa94466b
KingBoyAndGirl <1782635977@qq.com>
2023-10-16 17:33:01 +0800

    抖音和小红书居中显示

--- src/components/reception/footer.jsx
+++ src/components/reception/footer.jsx
@@ -1,23 +1,25 @@
 import '@/style/reception/footer.scss';
 
 const Footer = () => {
-  const douyinLink = "https://v.douyin.com/id5BSE8g/"; // 将 "抖音跳转链接" 替换为实际的抖音跳转链接
-  const xiaohongshuLink = "https://www.xiaohongshu.com/user/profile/5e2b0aa60000000001004d2b?xhsshare=CopyLink&appuid=5e2b0aa60000000001004d2b&apptime=1697446529"; // 将 "小红书跳转链接" 替换为实际的小红书跳转链接
+  const douyinLink = "https://v.douyin.com/id5BSE8g/";
+  const xiaohongshuLink = "https://www.xiaohongshu.com/user/profile/5e2b0aa60000000001004d2b?xhsshare=CopyLink&appuid=5e2b0aa60000000001004d2b&apptime=1697446529";
 
-  // Must add passHref to Link
+  const linkStyle = {
+    marginRight: '20px', // 增加更多的右侧间距
+  };
+
   return (
             <footer className='footer'>
               <div>
-                <div style={{ marginTop: '30px' }}>
+                <div style={{ marginTop: '5px' }}>
                   所有作品均由小王摄影拍摄: <br/>
-                  <a href={douyinLink} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'underline', color: '#ff0000' }}>
+                  <a href={douyinLink} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'underline', color: '#ff0000', ...linkStyle }}>
                     <i className="fab fa-tiktok"></i> 抖音：XWSY688
                   </a>
-
-                  <br/>
                   <a href={xiaohongshuLink} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'underline', color: '#ff0000' }}>
                     <i className="fab fa-xing"></i> 小红书：小王摄影
-                  </a>  <br/>
+                  </a>
+                  <br/>
                   转载必须征得本人同意后，方可转载！！！
                 </div>
                 <div>&copy; 小王摄影 2022-2023</div>
--- src/view/reception/about.jsx
+++ src/view/reception/about.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import dy from '@/assets/dy.jpg'
-import xhs from '@/assets/xhs.jpg'
+import dy from '@/assets/dy.jpg';
+import xhs from '@/assets/xhs.jpg';
 import logo from "@/assets/logo.png";
 
 const App = () => {
@@ -11,22 +11,23 @@ const App = () => {
       };
 
       return (
-                <div id="about" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-                      <div id="header" style={{ backgroundColor: '#ff9900', color: '#fff', textAlign: 'center', padding: '5% 0', width: '80%' }}>
-                            <h1>捕捉美好瞬间</h1>
-                            <p>眼睛不是摄影机终究会忘记，但是美丽的照片是永恒的</p>
-                      </div>
-                      <div id="promo-container" style={{ fontSize: '24px', backgroundColor: '#f2f2f2', textAlign: 'center', padding: '5% 0', width: '80%', margin: '0 auto' }}>
-                            <div className="promo-text" style={{ background: '#007bff', color: '#fff' }}>点击图片联系</div>
-                      </div>
-                      <div id="image" style={{ width: '80%',padding: '5%', display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-                            <Link to="https://v.douyin.com/id5BSE8g/">
-                                  <img src={dy} alt="点击跳转" style={imageStyle} />
-                            </Link>
-                            <Link to="https://www.xiaohongshu.com/user/profile/5e2b0aa60000000001004d2b?xhsshare=CopyLink&appuid=5e2b0aa60000000001004d2b&apptime=1697446529">
-                                  <img src={xhs} alt="点击跳转" style={imageStyle} />
-                            </Link>
-                      </div>
+                <div
+                          id="image"
+                          style={{
+                                width: '50%',
+                                display: 'flex',
+                                flexDirection: 'row',
+                                justifyContent: 'space-around',
+                                alignItems: 'center', // 居中垂直对齐
+                                padding: '0px 22%',
+                          }}
+                >
+                      <Link to="https://v.douyin.com/id5BSE8g/">
+                            <img src={dy} alt="点击跳转" style={imageStyle} />
+                      </Link>
+                      <Link to="https://www.xiaohongshu.com/user/profile/5e2b0aa60000000001004d2b?xhsshare=CopyLink&appuid=5e2b0aa60000000001004d2b&apptime=1697446529">
+                            <img src={xhs} alt="点击跳转" style={imageStyle} />
+                      </Link>
                 </div>
       );
 }
